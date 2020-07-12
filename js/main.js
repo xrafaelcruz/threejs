e1d74a128cb006adf7241f3bde09cfa22df506d2
Rafael Cruz <xrafaelcruz@gmail.com>
2020-07-12 02:33:01 -0300

    first tutorial, 3d cube

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/* 
+  I need to have three things to display something:
+  1 - Scene
+  2 - Camera
+  3 - Renderer
+*/
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube); // after .add, the default camera.position is (0, 0, 0)
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+animate();
